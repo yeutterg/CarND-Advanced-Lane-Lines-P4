f653e90a1da2c3b40a9c5064005a03e821fc81bc
Greg Yeutter <greg@Gregs-MacBook-Pro.local>
2017-05-13 20:49:39 -0400

    even better performance x3

--- main.py
+++ main.py
@@ -720,11 +720,11 @@ def img_process_pipeline(img, ksize=3, saveFile=0, fname='', smoothing=1):
             left_fit, right_fit = sliding_window(warped, hist)
     else: 
         # Perform a margin search with the previous fit values
-        left_fit, right_fit = margin_search(warped, left_fit_prev, right_fit_prev, margin=50)
+        left_fit, right_fit = margin_search(warped, left_fit_prev, right_fit_prev, margin=40)
 
     # Apply smoothing
     if smoothing:
-        left_fit, right_fit = fit_mvg_avg(left_fit, right_fit, num=3, diff=5e-4)
+        left_fit, right_fit = fit_mvg_avg(left_fit, right_fit, num=3, diff=4e-4)
 
     # Get the radius of curvature for both lines
     left_curverad, right_curverad, avg_curverad = radius_of_curvature(warped, left_fit, right_fit)
